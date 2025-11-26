d9050f51fe074d2ede04f3d58d0bf16c4641f908
Varya Stepanova <mail@varya.me>
2025-11-26 12:48:32 +0200

    grouping props

---
+++ .storybook/constants.ts
@@ -0,0 +1,8 @@
+export enum PropsCategory {
+    ATTRIBUTES = 'General',
+    EVENTS = 'Events',
+    ACCESSIBILITY = 'Accessibility',
+    VISUAL = 'Visual',
+    APPEARANCE = 'Appearance',
+    CONTENT = 'Content',
+  }
--- src/components/alert/alert.stories.tsx
+++ src/components/alert/alert.stories.tsx
@@ -1,6 +1,8 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { fn } from 'storybook/test';
 
+import { PropsCategory } from '@/.storybook/constants';
+
 import { Alert } from '@/src/components';
 
 
@@ -14,24 +16,45 @@ const meta: Meta<typeof Alert> = {
     variant: {
       control: 'select',
       options: ['Info', 'Warning', 'Success', 'Danger'],
+      table: {
+        category: PropsCategory.ACCESSIBILITY,
+      },
     },
     icon: {
       control: 'boolean',
+      table: {
+        category: PropsCategory.APPEARANCE,
+      },
     },
     link: {
       control: 'boolean',
+      table: {
+        category: PropsCategory.APPEARANCE,
+      },
     },
     dismissible: {
       control: 'boolean',
+      table: {
+        category: PropsCategory.APPEARANCE,
+      },
     },
     text: {
       control: 'text',
+      table: {
+        category: PropsCategory.CONTENT,
+      },
     },
     linkLabel: {
       control: 'text',
+      table: {
+        category: PropsCategory.CONTENT,
+      },
     },
     onDismiss: {
       action: 'dismissed',
+      table: {
+        category: PropsCategory.EVENTS,
+      },
     },
   },
   tags: ["autodocs"],
