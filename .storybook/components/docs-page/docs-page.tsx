338d1e98c25c884f1b8a90a46abd72c405955de3
Varya Stepanova <mail@varya.me>
2025-11-27 23:01:40 +0200

    custom docs page

---
+++ .storybook/components/docs-page/docs-page.tsx
@@ -0,0 +1,46 @@
+import React, { useContext }  from 'react';
+
+import {
+  Controls,
+  Stories,
+  DocsContext,
+  Subtitle,
+  Primary
+} from '@storybook/addon-docs/blocks';
+
+import { ComponentDescription } from '../component-description/component-description';
+import { ComponentTitle } from '../component-title/component-title';
+import { SectionHeading } from '../section-heading/section-heading';
+
+
+export const DocsPage = () => {
+
+  const context = useContext(DocsContext);
+
+  const componentStories = context.componentStories();
+
+  const stories = componentStories
+    .filter((story) => !story.parameters?.['docs']?.disable)
+    .slice(1);
+
+  const isSingleStory = Object.keys(stories).length === 1;
+
+  return (
+    <>
+      <ComponentTitle />
+      <Subtitle />
+      <SectionHeading>Overview</SectionHeading>
+      <ComponentDescription />
+      <Primary />
+      <SectionHeading>Props</SectionHeading>
+      <Controls />
+      {isSingleStory ? null : (
+        <>
+          <SectionHeading>Variants</SectionHeading>
+          <Stories />
+        </>
+      )}
+    </>
+  );
+};
+
