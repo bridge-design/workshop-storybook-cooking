230519b52a62023eeba70a252825b3e18a615f0a
Varya Stepanova <mail@varya.me>
2025-12-15 23:26:41 +0200

    Build fix

--- .storybook/constants.ts
+++ .storybook/constants.ts
@@ -1,6 +1,8 @@
 export enum PropsCategory {
-    ATTRIBUTES = 'General',
-    EVENTS = 'Events',
-    ACCESSIBILITY = 'Accessibility',
-    VISUAL = 'Visual',
-  }
+  ATTRIBUTES = 'General',
+  EVENTS = 'Events',
+  ACCESSIBILITY = 'Accessibility',
+  VISUAL = 'Visual',
+  APPEARANCE = 'Appearance',
+  CONTENT = 'Content',
+}
